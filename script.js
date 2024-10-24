8c2856c2bce7fd94a0ddc5a1bd3131b163724a83
Thariss <thariss.thavakumar@gmail.com>
2024-10-24 13:54:28 -0400

    Made my second calculator, currently fixing the css on my second calculator (Not upto scale); and still working on the javascript

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function Coslaw() {
     const a = parseFloat(document.getElementById("side_a").value);
     const b = parseFloat(document.getElementById("side_b").value);
-    const c = parseFloat(document.getElementById("side_c").value) * (Math.PI / 100);
+    const C = parseFloat(document.getElementById("Angle").value) * (Math.PI / 180);
 
     const c_length = Math.sqrt(a * a + b * b - (2 * a * b * Math.cos(C)));
-    document.getElementById("sidec").value = c_length.toFixed(2);
+    document.getElementById("c_length").value = c_length.toFixed(2);
 }
